4c0f2ca78702ba40624c2f3c33a8c9ca30f931d0
jilvesko <jukka.ilveskorpi@edu.sasky.fi>
2023-06-11 21:54:11 +0300

    2.6 - 2.9

--- Osa 2/phonebook/src/App.js
+++ Osa 2/phonebook/src/App.js
@@ -2,14 +2,15 @@
 import React, { useState } from 'react'
 import './App.css';
 import Person from './components/Person'
+<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 //import Notification from './components/Notification.js'
 
 const App =(props)=> {
   const [persons, setPersons] = useState(props.persons)
   const [newPerson, setNewPerson] = useState("") 
   const [newNumber, setNewNumber] = useState("")
-  const [newFilter, setNewFilter] = useState("")
-  const [errorMessage, setErrorMessage] = useState("")
+  const [filter, setFilter] = useState("")
+  //const [errorMessage, setErrorMessage] = useState("")
 
   const addPerson =(event)=> {
     event.preventDefault()
@@ -45,22 +46,35 @@ const App =(props)=> {
     setNewNumber(event.target.value)
   }
 
-  const filterPerson =()=> {
-    setFilterPerson(event.target.value)
-  }
-
+ /* const handlefiltered =(event)=> {
+    setFilter (event.target.value)
+  } */ 
+    
   return (
     <div className="App">
        <br></br><br></br><br></br>
        <h1>Phonebook</h1>
        <br></br>
-       Filter shown with <input value={filterPerson} />
-       <form onSubmit={addPerson}>
+
+       Filter shown with 
+        <div>
         <input 
+        type="text" 
+        placeholder="Search..." 
+        className="search" 
+        //value={filter} 
+        onChange={(e) => setFilter(e.target.value)} 
+        //onChange={handlefiltered}
+        />
+       </div>
+      
+       <br></br>
+       <form onSubmit={addPerson}>
+        Name <input 
         value={newPerson} 
         onChange={handlePersonChange}
         /><br></br>
-        <input 
+        Number <input 
         value={newNumber} 
         onChange={handleNumberChange}
         />
@@ -68,11 +82,13 @@ const App =(props)=> {
       </form> 
 
        <ul>
-         {persons.map(person =>
-          <Person key={person.id}
-           person={person} 
-            />
-          )}
+         {persons.filter((person) =>
+          person.name.toLowerCase().includes(filter))
+                .map((person) => (
+                <Person key={person.id}
+                 person={person} 
+              />
+          ))}
        </ul>
        <br></br><br></br><br></br><br></br><br></br><br></br>
     </div>
@@ -82,3 +98,31 @@ const App =(props)=> {
 export default App;
 
  // <Notification message={errorMessage} />
+
+ /*
+personsToShow =
+		newFilter === ''
+			? (personsToShow = persons)
+			: (personsToShow = persons.filter(person =>
+					person.name.toLowerCase().includes(newFilter.toLowerCase())
+			  ))
+
+	<PersonForm
+				newName={newName}
+				setNewName={setNewName}
+				newNumber={newNumber}
+				setNewNumber={setNewNumber}
+				persons={persons}
+				setPersons={setPersons}
+				setMessage={setMessage}
+				setMessageType={setMessageType}
+				message={message}
+			/>
+			<h2>Numbers</h2>
+			<Persons
+				personsToShow={personsToShow}
+				setPerson={setPersons}
+				handleDelete={handleDelete}
+				persons={persons}
+			/>
+ */
